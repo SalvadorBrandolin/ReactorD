6e6bf4e265e694d92f88deddb8fc7ea72e1d0751
salvadorbrandolin <salvadorebrandolin@gmail.com>
2022-08-22 22:41:21 -0300

    Implemented thermo for critical properties

--- reactord/Substance.py
+++ reactord/Substance.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from re import T
 import numpy as np
+import thermo.chemical
 
 class Substance:
     """"Substance" object defining class
@@ -7,15 +9,29 @@ class Substance:
     Parameters
     ----------
 
-    Name : string
+    name : string
          Name of the substance.
-    Mw : float
+    mw : float
          Molecular weight of the subtance [g / mol].
     """
 
-    #ESTA ES LA BRANCH DEVELOPMENT
+    def __init__(self, id=None, name=None, mw=None, Tc=None, Pc=None, w=None,
+                VolumeLiquid_func):
 
-    def __init__(self, Name = None, Mw = None):
+        if id == None:
+          pass
+        else:
+            self.thermo_chemical_object = thermo.chemical.Chemical(ID = id)
 
-        self.Name = Name
-        self.Mw = Mw        
+            #Pure compound properties:
+            self.name = self.thermo_chemical_object.name
+            self.mw = self.thermo_chemical_object.MW
+            self.Tc = self.thermo_chemical_object.Tc
+            self.Pc = self.thermo_chemical_object.Pc
+            self.omega = self.thermo_chemical_object.omega
+
+            #Temperature dependent properties calculation functions:
+            VolumeLiquid_func = self.thermo_chemical_object.VolumeLiquid
+
+    def VolumeLiquid(self, VolumeLiquid_func, T):
+        return VolumeLiquid_func(T)
