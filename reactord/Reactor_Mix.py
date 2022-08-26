c875194b46f56e8366dbf1b23ccf4bef6bbabf4f
Adrieto <parodiadrian3@gmail.com>
2022-08-26 17:02:00 -0300

    Se agregó el archivo Reactor_mix.py

--- reactord/Reactor_Mix.py
+++ reactord/Reactor_Mix.py
@@ -1,12 +1,39 @@
+import numpy as np
+
 class Mix:
     """
     """ 
 
-    def __init__(self, component_list, moles, phase):
+    def __init__(self, component_list, moles, phase, volume=None, Temp, Presure=None):
         self.phase= phase.lower()
         self.component_list= component_list
         self.moles= moles
+        self.volume= volume
+        self.concentrations= np.zeros (len(self.component_list))
 
-        if self.phase== 'liquid':
-            for idx, components in enumerate (self.component_list):
-                self.concentrations[idx]=
+        #Initial concentrations in liquid phase:
+        if self.phase == 'liquid' and self.volume > 0:
+            for idx, component in enumerate (self.component_list):
+                self.concentrations[idx]= moles[idx] / self.volume
+
+                #ACA SE PUEDE PONER UNA EXCEPCION SI EL VOLUMEN ES NEGATIVO O CERO (OJO CON ESTO!!)
+        
+    
+        #Initial concentrations in gas phase:
+        elif self.phase == 'gas' and self.volume > 0:
+            for idx, component in enumerate (self.component_list):
+                self.concentrations[idx]= Pressure[idx] / self.volume
+
+
+        elif self.volume <= 0:
+            print ('Volume must be a positive number') 
+            
+
+
+            
+
+
+
+Mezcla= Mix(['water','methane'], [1,5], 'LiQuiD', volume=0)
+print (Mezcla.concentrations, type(Mezcla.concentrations))
+
