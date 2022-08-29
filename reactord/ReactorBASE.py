20f8584bb2c87852ef0811b693cc7ae24179f266
salvadorbrandolin <salvadorebrandolin@gmail.com>
2022-08-29 20:06:42 -0300

    Corrections over ReactorBase class

--- reactord/ReactorBASE.py
+++ reactord/ReactorBASE.py
@@ -16,25 +16,25 @@ class PDEReactorBase(PDEBase):
         self.P = P
         self.N = Ns
         self.x_span = x_spans
-        #self.f_in = flow_mixer(f_ins) #PROGRAMAR UN MEZCLADOR DE ENTRADAS
-        self.f_out = f_out
+        #f_in = flow_mixer(f_ins) #PROGRAMAR UN MEZCLADOR DE ENTRADAS
+        f_out = f_out
         
         self.in_flows = []
-        if self.fin != None:
-            self.in_flows.append(flow for flow in fin.molar_flows)
+        if f_in != None:
+            self.in_flows.append(flow for flow in f_in.molar_flows)
         else:
-            self.in_flows = np.zeros(self.n_substances)
+            raise Exception("Input inlet molar flow currents")
 
         self.out_flows = []
-        if self.out != None:
-            self.in_flows.append(flow for flow in self.f_out.molar_flows)
+        if f_out != None:
+            self.out_flows.append(flow for flow in self.f_out.molar_flows)
         else:
-            self.in_flows = np.zeros(self.n_substances)
+            raise Exception("Input output molar flow currents")
 
     def grid_generator(self):
         return None
 
-    def state_generator(self):
+    def initial_state_generator(self):
         return None
 
     def mass_balance(self):
