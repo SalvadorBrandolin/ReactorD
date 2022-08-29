460e40cb47bd021215add83531c904dcd251fb98
salvadorbrandolin <salvadorebrandolin@gmail.com>
2022-08-29 19:56:10 -0300

    General reactor class

--- reactord/ReactorBASE.py
+++ reactord/ReactorBASE.py
@@ -1,18 +1,53 @@
+import numpy as np
+#import FinsMixer
 from pde import PDEBase
 
-class ReactorBASE(PDEBase):
-    def __init__(self):
-        #COSAS
-        return
-    
-    def grid_state_gen(self):
-        return
+class PDEReactorBase(PDEBase):
+    def __init__(self, kinetic, x_spans=None, Ns=None, N0=None, Nf=None, 
+        f_ins=None, f_out=None, kinetic_arg=None, operation=None, T=None, 
+        P=None,  
+        ):
 
-    def evolution_rate(self, state: FieldBase, t: float = 0) -> FieldBase:
-        return super().evolution_rate(state, t)
+        self.n_substances = len(kinetic)
+        self.substances = kinetic.substances
+        self.kinetic = kinetic
+        self.operation = operation
+        self.T = T
+        self.P = P
+        self.N = Ns
+        self.x_span = x_spans
+        #self.f_in = flow_mixer(f_ins) #PROGRAMAR UN MEZCLADOR DE ENTRADAS
+        self.f_out = f_out
+        
+        self.in_flows = []
+        if self.fin != None:
+            self.in_flows.append(flow for flow in fin.molar_flows)
+        else:
+            self.in_flows = np.zeros(self.n_substances)
+
+        self.out_flows = []
+        if self.out != None:
+            self.in_flows.append(flow for flow in self.f_out.molar_flows)
+        else:
+            self.in_flows = np.zeros(self.n_substances)
+
+    def grid_generator(self):
+        return None
+
+    def state_generator(self):
+        return None
+
+    def mass_balance(self):
+        return None
+
+    def energy_balance(self):
+        return None
+
+    def pressure_balance(self):
+        return None
 
     def solve(self):
-        return
-
-
+        return None
     
+    def outputs(self):
+        return None
