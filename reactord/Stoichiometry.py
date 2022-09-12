1eda3493d6772e18b1195f3caa131eed1e69c900
Maricel <marisantosvg@gmail.com>
2022-09-12 12:09:40 -0300

    new commit

--- reactord/Stoichiometry.py
+++ reactord/Stoichiometry.py
@@ -31,18 +31,29 @@ class Stoichiometry:
         self.names= names 
 
         #The coefficients array is created:        
-        self.coefficients = input(f"Introduce the matrix of coefficients with "
+        self.coefficients = np.array(eval((input(f"Introduce the matrix of coefficients with "
             f"{self.reactions} files and {self.total_comp} columns."
-            f"\nUse negative numbers for reactants and positive for products:")
+            f"\nUse negative numbers for reactants and positive for products:"))))
+        #self.coefficients= np.array(coefficients)
            
     def __str__(self):
         return(f"Total components: {str(self.total_comp)}\n"
         f"Number of reactions: {str(self.reactions)} \n")
 
+    def __iter__(self):
+      return self.coefficients.__iter__()
+    
+    def __len__(self):
+        return len(self.coefficients)
+    #selecctor de elemento dentro del objeto
+    def __getitem__(self, index):
+        return self.coefficients[index]
+    #buscador de elemento detro del objeto
+    
 
 
 # ACA SE PRUEBA LA CLASE    
-#prueba= Stoichiometry(2,2,["reaccion1", "esterficacion"])
-#print(prueba.coefficients) #La matriz de coeficientes
+#prueba= Stoichiometry(2,2)
+#print(prueba.coefficients,np.shape(prueba.coefficients)) #La matriz de coeficientes
 #print(prueba.names) #Nombres de las reacciones
 #print(prueba) #Se prueba el metodo __str__
