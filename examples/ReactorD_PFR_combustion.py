e1a55b77eebb1a0240535e34897121358fe7764f
Adrieto <parodiadrian3@gmail.com>
2022-11-27 22:53:33 -0300

    Se agregó un ejemplo de uso del PFR isoT/isoBar. La ejecucion falla.

---
+++ examples/ReactorD_PFR_combustion.py
@@ -0,0 +1,44 @@
+import numpy as np
+import reactord as rd
+
+# Example: methane combustion reaction
+# CH4 + 2 O2 ----> 2 H2O + CO2
+# A  + 2B  ----> 2C + D
+
+
+# We create the substance objects:
+A= rd.Substance.from_thermo_database("methane")
+B= rd.Substance.from_thermo_database("oxygen")
+C= rd.Substance.from_thermo_database("water")
+D= rd.Substance.from_thermo_database("co2")
+
+# Then we create the mixture:
+mixture = rd.mix.IdealGas(([A,B,C,D]))
+
+# and the stoichiometry matrix:
+stoichiometry = np.array ([[-1, -2, 2, 1]])
+
+# A function for the reaction rate is defined:
+def rate1(Ca,Cb):
+    return 10000*Ca*Cb
+
+# Kinetics is instantiated and "concentration" are being used to make
+# the calculations
+kinetics = rd.Kinetics (mixture, [rate1], stoichiometry, "concentration")
+
+# An instance of the stationary plug flow reactor class is instantiated
+# at isothermic and isobaric conditions:
+PFR = rd.idealreactor.StationaryPFR.set_isothermic_isobaric(
+    mix=mixture, 
+    list_of_reactions=[rate1],
+    stoichiometry=stoichiometry,
+    kinetic_argument="partial_pressure",
+    reactor_dim_minmax=[0,1],
+    transversal_area=1,
+    isothermic_temperature=350,
+    isobaric_pressure=5*101325, 
+    molar_flow_in={"methane": 100, "oxygen": 100, "water":0, "carbon dioxide":0},
+    )
+
+# Simulation:
+solution= PFR.simulate() #Falla la simulacion por un error en el calculo de velocidades de reaccion
