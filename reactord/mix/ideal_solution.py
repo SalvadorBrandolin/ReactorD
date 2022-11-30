bf5e6c30a9331992504380fb4505db664e8381fe
salvadorbrandolin <salvadorebrandolin@gmail.com>
2022-11-30 10:01:38 -0300

    style error

--- reactord/mix/ideal_solution.py
+++ reactord/mix/ideal_solution.py
@@ -26,7 +26,9 @@ class IdealSolution(AbstractMix):
     def __init__(self, substance_list: List[Substance]):
         self.substances = substance_list
 
-    def concentrations(self, moles: List[float], temperature: float, pressure: float) -> List[float]:
+    def concentrations(
+        self, moles: List[float], temperature: float, pressure: float
+    ) -> List[float]:
         """Calculate concentrations of the mixture.
 
         Parameters
