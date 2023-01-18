7dee773e6dd19957b553a21b7bbfdad1b42fbabc
Salvador <salvadorebrandolin@gmail.com>
2023-01-18 18:39:51 -0300

    style errors

--- reactord/substance.py
+++ reactord/substance.py
@@ -326,7 +326,7 @@ class Substance:
         ----------
         name_file : str
             Name of the file.
-        
+
         Returns
         -------
         Substance : Substance
@@ -347,7 +347,7 @@ class Substance:
         ----------
         name_file : str
             Name of file to save the substance object.
-        
+
         Returns
         -------
         _file_
